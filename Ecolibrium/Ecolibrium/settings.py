15f73d08c7545e7bd088fa3d9824cea9915c381e
vikash jakapure <57489103+VikashJakapure@users.noreply.github.com>
2020-07-16 16:57:35 +0530

    Update settings.py

--- Ecolibrium/Ecolibrium/settings.py
+++ Ecolibrium/Ecolibrium/settings.py
@@ -82,6 +82,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'acueasy',
+    #     'USER' : 'root',
+    #     'PASSWORD' : 'root',
+    #     'HOST' : 'localhost',
+    #     'PORT' : '3306',
+    # }
     }
 }
 
